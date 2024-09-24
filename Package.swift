10a2059c84e6519c82765b0b0d108866832bbc73
Nidal Fakhouri <nfakhouri@stytch.com>
2024-09-24 15:52:20 -0400

    update package.swift

--- Package.swift
+++ Package.swift
@@ -4,22 +4,26 @@
 import PackageDescription
 
 let package = Package(
-    name: "StytchDFPPublic",
+    name: "StytchDFP",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "StytchDFPPublic",
-            targets: ["StytchDFPPublic"]),
+            name: "StytchDFP",
+            targets: ["stytch-ios-dfp"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "StytchDFPPublic"
+            name: "stytch-ios-dfp",
+            dependencies: [
+              "StytchDFP"
+            ],
+            publicHeadersPath: "."
         ),
         .binaryTarget(
             name: "StytchDFP",
-            path: "./Sources/StytchDFPPublic/StytchDFP.xcframework"
+            path: "./Sources/stytch-ios-dfp/StytchDFP.xcframework"
         )
     ]
 )
