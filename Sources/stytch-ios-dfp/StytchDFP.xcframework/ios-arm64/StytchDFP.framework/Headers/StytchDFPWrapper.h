59a1883dd6c882153d834b81890a076c694c54a1
Nidal Fakhouri <nfakhouri@stytch.com>
2025-04-01 13:11:46 -0400

    Update framework build

--- Sources/stytch-ios-dfp/StytchDFP.xcframework/ios-arm64/StytchDFP.framework/Headers/StytchDFPWrapper.h
+++ Sources/stytch-ios-dfp/StytchDFP.xcframework/ios-arm64/StytchDFP.framework/Headers/StytchDFPWrapper.h
@@ -2,7 +2,15 @@
 
 @interface StytchDFP : NSObject
 
-- (NSString *)getTelemetryID;
-- (NSString *)processString:(NSString *)input;
+typedef void (^TelemetryIDCompleteCallbackType)(NSString * _Nonnull telemetry_id); //Declare the block type
+
+@property (nonatomic, readonly, strong) NSString * _Nonnull PublicToken;
+@property (nonatomic, readonly, strong) NSString * _Nullable SubmitURL;
+@property TelemetryIDCompleteCallbackType _Nonnull TelemetryIDCompleteCallback; //Declare the block property using the block type
+
+- (void)getTelemetryID:(TelemetryIDCompleteCallbackType _Nonnull)callback;
+
+- (void)configureWithPublicToken:(NSString * _Nonnull)publicToken
+                       submitURL:(NSString * _Nullable)submitURL;
 
 @end
